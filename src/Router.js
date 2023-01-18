1ea7cf2280d0d530fc639524882d1814cd23d977
Ha-Pyeongan <96smilepeace@gmail.com>
2023-01-18 16:42:14 +0900

    ADD: styles-reset.css pull -> merge feature/Nav_layout

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main/Main";
+import Nav from "./Components/Nav/Nav";
 
 export default function Router() {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/" element={<Main />} />
       </Routes>
