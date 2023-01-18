6de077b7ad0718b6c64e1c3b3b6eacb106a93215
Mia <tjwlfif@gmail.com>
2023-01-18 18:15:31 +0900

    Add: initial style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./Router";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./Styles/GlobalStyle";
+import theme from "./Styles/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Router />);
+root.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <Router />
+  </ThemeProvider>
+);
