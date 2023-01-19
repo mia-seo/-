8bb08e782225ae0e9b560e535caa0435a29d6e43
Mia <tjwlfif@gmail.com>
2023-01-19 10:38:12 +0900

    Add: Chat title 구현

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import Router from "./Router";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./Styles/GlobalStyle";
+import variables from "./Styles/variables";
 import theme from "./Styles/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ThemeProvider theme={theme}>
+  <ThemeProvider theme={{ style: theme, variables }}>
     <GlobalStyle />
     <Router />
   </ThemeProvider>
