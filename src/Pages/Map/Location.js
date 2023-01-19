40842f9f5cc1ae9ba615a73a25b9172958f1e413
baegyungmin <bjy6439@naver.com>
2023-01-19 18:17:11 +0900

    Add : 상점 클릭시 해당 좌표로 이동 및 마크 출력기능 구현

--- src/Pages/Map/Location.js
+++ src/Pages/Map/Location.js
@@ -1,28 +1,59 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 const { kakao } = window;
 
-const Location = () => {
-  const [map, setMap] = useState(null);
+const Location = ({ longitude, latitude }) => {
+  let imageSrc =
+    "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
 
   //처음 지도 그리기
   useEffect(() => {
-    const container = document.getElementById("map");
-    const options = { center: new kakao.maps.LatLng(33.450701, 126.570667) };
-    const kakaoMap = new kakao.maps.Map(container, options);
-    setMap(kakaoMap);
-  }, []);
+    let mapContainer = document.getElementById("map"), // 지도를 표시할 div
+      mapOption = {
+        center: new kakao.maps.LatLng(
+          latitude ?? 37.506307228504966,
+          longitude ?? 127.05371643215041
+        ), // 지도의 중심좌표
+        level: 3, // 지도의 확대 레벨
+      };
+
+    let map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+    let positions = [
+      {
+        title: "위코드",
+        latlng: new kakao.maps.LatLng(37.506307228504966, 127.05371643215041),
+      },
+      {
+        title: "옆가게",
+        latlng: new kakao.maps.LatLng(latitude, longitude),
+      },
+    ];
+
+    for (let i = 0; i < positions.length; i++) {
+      // 마커 이미지의 이미지 크기 입니다
+      let imageSize = new kakao.maps.Size(24, 35);
+
+      // 마커 이미지를 생성합니다
+      let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+      // 마커를 생성합니다
+      let marker = new kakao.maps.Marker({
+        map: map, // 마커를 표시할 지도
+        position: positions[i].latlng, // 마커를 표시할 위치
+        title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+        image: markerImage, // 마커 이미지
+      });
+    }
+  });
 
   return (
     <div
       style={{
         width: "100%",
         display: "inline-block",
-        marginLeft: "5px",
-        marginRight: "5px",
       }}
     >
-      <div id="map" style={{ width: "99%", height: "500px" }} />
+      <div id="map" style={{ width: "100%", height: "500px" }} />
     </div>
   );
 };
--- src/pages/Map/Map.js
+++ src/pages/Map/Map.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Location from "./Location";
 import * as M from "./Map.styles";
 import "../../styles/reset.css";
 
 const Map = () => {
+  const [items, setItems] = useState([]);
+  const [latitude, setLatitude] = useState();
+  const [longitude, setLongitude] = useState();
+
+  const addMapPoint = (lat, lon) => {
+    setLatitude(lat);
+    setLongitude(lon);
+  };
+
+  useEffect(() => {
+    fetch("/data/itemdata.json")
+      .then(res => res.json())
+      .then(data => setItems(data));
+  }, []);
+
   return (
     <M.Container>
       <M.Fiter>
@@ -20,40 +35,28 @@ const Map = () => {
       </M.Fiter>
       <M.ContainerBox>
         <M.Inner>
-          <Location />
+          <Location latitude={latitude} longitude={longitude} />
         </M.Inner>
         <M.List>
-          <M.Lists>
-            <M.ItemImg src="/images/cake.jpg" />
-            <M.ItemDec>
-              <M.ItmeInfo primary>상호명 : 굽네치킨</M.ItmeInfo>
-              <M.ItmeInfo>주소 : 서울특별시 서대문구 충정로 43-1</M.ItmeInfo>
-              <M.ItmeInfo>전화번호 : 02-433-9292</M.ItmeInfo>
-              <M.ItmeInfo>대표메뉴 : 치킨</M.ItmeInfo>
-              <M.ItmeInfo>카테고리 : 인스턴트</M.ItmeInfo>
-              <M.ItmeInfo>가격대 : 1만원 대</M.ItmeInfo>
-            </M.ItemDec>
-          </M.Lists>
-          <M.Lists primary>
-            <M.ItemImg src="/images/cake.jpg" />
-            <M.ItemDec>
-              <M.ItmeInfo primary>상호명 : 굽네치킨</M.ItmeInfo>
-              <M.ItmeInfo>주소 : 서울특별시 서대문구 충정로 43-1</M.ItmeInfo>
-              <M.ItmeInfo>전화번호 : 02-433-9292</M.ItmeInfo>
-              <M.ItmeInfo>대표메뉴 : 치킨</M.ItmeInfo>
-              <M.ItmeInfo>카테고리 : 인스턴트</M.ItmeInfo>
-            </M.ItemDec>
-          </M.Lists>
-          <M.Lists>
-            <M.ItemImg src="/images/cake.jpg" />
-            <M.ItemDec>
-              <M.ItmeInfo primary>상호명 : 굽네치킨</M.ItmeInfo>
-              <M.ItmeInfo>주소 : 서울특별시 서대문구 충정로 43-1</M.ItmeInfo>
-              <M.ItmeInfo>전화번호 : 02-433-9292</M.ItmeInfo>
-              <M.ItmeInfo>대표메뉴 : 치킨</M.ItmeInfo>
-              <M.ItmeInfo>카테고리 : 인스턴트</M.ItmeInfo>
-            </M.ItemDec>
-          </M.Lists>
+          {items.map((i, index) => {
+            return (
+              <M.Lists
+                key={index}
+                data-latitude={i.latitude}
+                onClick={() => addMapPoint(i.latitude, i.longitude)}
+              >
+                <M.ItemImg src="/images/cake.jpg" />
+                <M.ItemDec>
+                  <M.ItmeInfo primary>상호명 : {i.name}</M.ItmeInfo>
+                  <M.ItmeInfo>주소 : {i.address}</M.ItmeInfo>
+                  <M.ItmeInfo>전화번호 : {i.Tel}</M.ItmeInfo>
+                  <M.ItmeInfo>대표메뉴 : {i.smenu}</M.ItmeInfo>
+                  <M.ItmeInfo>카테고리 : {i.category}</M.ItmeInfo>
+                  <M.ItmeInfo>가격대 : 1만원 대</M.ItmeInfo>
+                </M.ItemDec>
+              </M.Lists>
+            );
+          })}
         </M.List>
       </M.ContainerBox>
     </M.Container>
