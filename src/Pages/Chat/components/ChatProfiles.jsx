b0edaa6f1eaef93daa24d00917d17d2ba77e30a5
Mia <tjwlfif@gmail.com>
2023-01-22 13:37:53 +0900

    Add: profile 추가

--- src/Pages/Chat/components/ChatProfiles.jsx
+++ src/Pages/Chat/components/ChatProfiles.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as P from "./ChatProfiles.styles";
+import "./ChatProfiles.styles";
 
 const ChatProfiles = () => {
+  const [profiles, setProfiles] = useState();
+
+  useEffect(() => {
+    fetch("/Data/profiles.json")
+      .then(res => res.json())
+      .then(result => setProfiles(result));
+  }, []);
+
   return (
     <P.Profiles>
-      <P.Profile>백종호</P.Profile>
+      {profiles.map(profile => (
+        <P.Profile key={profile.id}>
+          <img src={profile.img_url} alt="프로필" />
+          <div>{profile.name}</div>
+        </P.Profile>
+      ))}
     </P.Profiles>
   );
 };
--- src/Pages/Chat/components/ChatProfiles.styles.js
+++ src/Pages/Chat/components/ChatProfiles.styles.js
@@ -2,10 +2,29 @@ import styled from "styled-components";
 
 export const Profiles = styled.div`
   ${props => props.theme.variables.wh("90vw", "30vh")};
-  background-color: green;
+  ${props => props.theme.variables.flex("", "", "")};
+  /* ${props => props.theme.style.radius};
+  background-color: white; */
+  gap: 10px;
 `;
 
 export const Profile = styled.div`
   ${props => props.theme.variables.wh("15vw", "30vh")};
-  background-color: red;
+  ${props => props.theme.variables.flex("column", "space-between", "center")};
+  ${props => props.theme.style.radius};
+  background-color: ${props => props.theme.style.lightTomato};
+
+  img {
+    ${props => props.theme.variables.wh("10vw", "20vh")};
+    border-radius: 50%;
+    margin-top: 20px;
+  }
+
+  div {
+    /* ${props => props.theme.variables.wh("10vw", "10vh")}; */
+    font-size: 20px;
+    font-weight: bold;
+    margin-bottom: 15px;
+    color: white;
+  }
 `;
