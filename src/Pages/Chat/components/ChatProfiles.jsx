91cbec4f17f78d20d81ae53aedbc924b7a26f1f4
Mia <tjwlfif@gmail.com>
2023-01-23 15:39:58 +0900

    Add: profile carousel 기능 추가

--- src/Pages/Chat/components/ChatProfiles.jsx
+++ src/Pages/Chat/components/ChatProfiles.jsx
@@ -1,9 +1,27 @@
 import React, { useEffect, useState } from "react";
+import { BiChevronLeftCircle, BiChevronRightCircle } from "react-icons/bi";
 import * as P from "./ChatProfiles.styles";
 import "./ChatProfiles.styles";
 
 const ChatProfiles = () => {
   const [profiles, setProfiles] = useState([]);
+  const [px, setPx] = useState(0);
+
+  const CARDWITH = 10.5;
+  const overFlowValue =
+    profiles.length < 10 ? 0 : (profiles.length - 9) * CARDWITH;
+
+  const handleClickLeft = () => {
+    if (px <= -overFlowValue) {
+      setPx(prev => prev + CARDWITH);
+    }
+  };
+
+  const handleClickRight = () => {
+    if (px >= -overFlowValue) {
+      setPx(prev => prev - CARDWITH);
+    }
+  };
 
   useEffect(() => {
     fetch("/Data/profiles.json")
@@ -12,14 +30,32 @@ const ChatProfiles = () => {
   }, []);
 
   return (
-    <P.Profiles>
-      {profiles.map(({ id, img_url, name }) => (
-        <P.Profile key={id}>
-          <img src={img_url} alt="프로필" />
-          <div>{name}</div>
-        </P.Profile>
-      ))}
-    </P.Profiles>
+    <P.ChatProfiles>
+      {profiles.length > 9 ? (
+        <>
+          <BiChevronLeftCircle
+            size="38px"
+            className="leftBtn"
+            onClick={handleClickLeft}
+          />
+          <BiChevronRightCircle
+            size="38px"
+            className="rightBtn"
+            onClick={handleClickRight}
+          />
+        </>
+      ) : null}
+      <P.CarouselContainer>
+        <P.Profiles style={{ transform: `translate(${px}vw)` }}>
+          {profiles.map(({ id, img_url, name }) => (
+            <P.Profile key={id}>
+              <img src={img_url} alt="프로필" />
+              <div>{name}</div>
+            </P.Profile>
+          ))}
+        </P.Profiles>
+      </P.CarouselContainer>
+    </P.ChatProfiles>
   );
 };
 
--- src/Pages/Chat/components/ChatProfiles.styles.js
+++ src/Pages/Chat/components/ChatProfiles.styles.js
@@ -1,13 +1,43 @@
 import styled from "styled-components";
 
-export const Profiles = styled.div`
+export const ChatProfiles = styled.div`
+  position: relative;
+
+  .leftBtn {
+    position: absolute;
+    z-index: 2;
+    top: 13vh;
+    left: -20px;
+    background-color: white;
+    color: ${props => props.theme.style.lightTomato};
+    border-radius: 50%;
+  }
+
+  .rightBtn {
+    position: absolute;
+    z-index: 2;
+    top: 13vh;
+    right: -20px;
+    background-color: white;
+    color: ${props => props.theme.style.lightTomato};
+    border-radius: 50%;
+  }
+`;
+
+export const CarouselContainer = styled.div`
   ${props => props.theme.variables.wh("90vw", "30vh")};
+  overflow: hidden;
+`;
+
+export const Profiles = styled.div`
   ${props => props.theme.variables.flex("", "", "")};
+  /* transform: translate(${props => props.changePx}vw); */
   gap: 10px;
+  transition: transform 1s;
 `;
 
 export const Profile = styled.div`
-  ${props => props.theme.variables.wh("15vw", "30vh")};
+  ${props => props.theme.variables.wh("20vw", "30vh")};
   ${props => props.theme.variables.flex("column", "space-between", "center")};
   ${props => props.theme.style.radius};
   background-color: ${props => props.theme.style.lightTomato};
